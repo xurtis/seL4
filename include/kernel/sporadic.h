365231f7e4570eb1605236cb14663bdf3aadf8cc
Curtis Millar <curtis.millar@data61.csiro.au>
2019-11-19 15:58:19 +1100

    mcs: Prevent binding SCs to blocked TCBs
    So as to ensure that all TCBs in queues are either faulted, passive, or
    have schedulable SCs we cannot allow arbitrary SCs to be bound to TCBs
    that are already in queues.
    
    If a TCB is in a queue it should first be removed before having an SC
    bound to it.
    
    An additional checking function is added to check for whether an SC has
    been configured.

--- include/kernel/sporadic.h
+++ include/kernel/sporadic.h
@@ -168,6 +168,15 @@ static inline bool_t refill_ready(sched_context_t *sc)
     return REFILL_HEAD(sc).rTime <= (NODE_STATE(ksCurTime) + getKernelWcetTicks());
 }
 
+/*
+ * Return true if an SC has been successfully configured with parameters
+ * that allow for a thread to run.
+ */
+static inline bool_t sc_active(sched_context_t *sc)
+{
+    return sc->scRefillMax > 0;
+}
+
 /* Create a new refill in a non-active sc */
 void refill_new(sched_context_t *sc, word_t max_refills, ticks_t budget, ticks_t period);
 
