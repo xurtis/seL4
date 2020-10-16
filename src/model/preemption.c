b7b97ed9129c11bf4a503a635620e3755f1a30cf
Curtis Millar <curtis.millar@data61.csiro.au>
2020-10-16 15:12:54 +1100

    Don't assume active SC at preemption

--- src/model/preemption.c
+++ src/model/preemption.c
@@ -33,7 +33,7 @@ exception_t preemptionPoint(void)
 #ifdef CONFIG_KERNEL_MCS
         } else {
             updateTimestamp();
-            if (!checkBudget()) {
+            if (!(NODE_STATE(ksCurSC)->scRefillMax && checkBudget())) {
                 return EXCEPTION_PREEMPTED;
             }
 #endif
